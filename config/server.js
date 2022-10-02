27aea407c4c55655c7f804098692c54140440724
Daniel_Sanabria06 <sanabria3210@gmail.com>
2022-10-01 19:08:20 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a94e73176acefa43aa1f51bb1185e454'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'edc79145-6a89-411a-8a3c-2400fca2e9bc'
+};
